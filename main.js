931194d1486a546f5bb76029bf930db3bfa053a9
kuznetsovandrey76 <and.rey.q@yandex.ru>
2016-12-02 21:51:17 +0300

    go to funny-math

--- main.js
+++ main.js
@@ -1,77 +1,35 @@
 ymaps.ready(init);
-    var myMap,
-        kuznetsovandrey76,
-        andrei_u,
-        sangeethasundar,
-        curiouscc,
-        lesedi;
-  	
+    var myMap, campers;
+    var kuznetsovandrey76, andrei_u, sangeethasundar, curiouscc, lesedi, heinhoang, eightthday, alecia;
+    var size = 30;
 
 function init(){     
     myMap = new ymaps.Map("map", {
-        center: [39.07, 21.17],
+        center: [30.00, 0.00],
         zoom: 3,
         controls: ['zoomControl', 'fullscreenControl']
     });
 
-// (start) how to add loop in this block?
-
-    kuznetsovandrey76 = new ymaps.Placemark([57.185569, 39.396566], {
-    	hintContent: '@kuznetsovandrey76', 
-        balloonContent: 'Andrey Kuznetsov<br>UTC +3:00' 
-    }, {
-    iconLayout: 'default#image',
-    iconImageHref: 'img/kuznetsovandrey76.png',
-    iconImageSize: [50, 50],
-    // iconImageOffset: [-3, -42]        
-    });
-
-    andrei_u = new ymaps.Placemark([45.748900, 21.208700], {
-    	hintContent: '@andrei_u', 
-        balloonContent: 'Uzun Andrei' 
-    }, {
-    iconLayout: 'default#image',
-    iconImageHref: 'img/andrei_u.png',
-    iconImageSize: [50, 50],
-    iconImageOffset: [-3, -42]        
-    });
+    kuznetsovandrey76 = new ymaps.Placemark([57.185569, 39.396566], {hintContent: '@kuznetsovandrey76', balloonContent: 'Andrey Kuznetsov<br>UTC +3:00' }, 
+                                                                    {iconImageHref: 'img/kuznetsovandrey76.png', iconLayout: 'default#image', iconImageSize: [size, size]});
+             andrei_u = new ymaps.Placemark([45.748900, 21.208700], {hintContent: '@andrei_u', balloonContent: 'Uzun Andrei'}, 
+                                                                    {iconLayout: 'default#image', iconImageHref: 'img/andrei_u.png', iconImageSize: [size, size]});
+      sangeethasundar = new ymaps.Placemark([13.085716, 80.208596], {hintContent: '@sangeethasundar', balloonContent: 'Sangeetha Sundar' }, 
+                                                                    {iconLayout: 'default#image', iconImageHref: 'img/sangeethasundar.png', iconImageSize: [size, size]});
+            curiouscc = new ymaps.Placemark([51.538417, -0.009306], {hintContent: '@curiouscc', balloonContent: 'Silvana P'}, 
+                                                                    {iconLayout: 'default#image', iconImageHref: 'img/curiouscc.png', iconImageSize: [size, size]});
+               lesedi = new ymaps.Placemark([-33.918861, 18.423300], {hintContent: '@lesedi', balloonContent: 'Lesedi Diamond Damane'}, 
+                                                                    {iconLayout: 'default#image', iconImageHref: 'img/lesedi.png', iconImageSize: [size, size]});
+	        heinhoang = new ymaps.Placemark([10.691048, 107.246215], {hintContent: '@heinhoang', balloonContent: 'Hein Hoang'}, 
+                                                                    {iconLayout: 'default#image', iconImageHref: 'img/heinhoang.png', iconImageSize: [size, size]});
+           eightthday = new ymaps.Placemark([52.9835189, -0.0179148], {hintContent: '@8thday', balloonContent: 'Toni Gibbons'}, 
+                                                                    {iconLayout: 'default#image', iconImageHref: 'img/eightthday.png', iconImageSize: [size, size]});
+               alecia = new ymaps.Placemark([32.217700, -82.413500], {hintContent: '@alecia', balloonContent: 'Toni Gibbons'}, 
+                                                                    {iconLayout: 'default#image', iconImageHref: 'img/alecia.png', iconImageSize: [size, size]});
     
-	sangeethasundar = new ymaps.Placemark([13.085716, 80.208596], {
-    	hintContent: '@sangeethasundar', 
-        balloonContent: 'Sangeetha Sundar' 
-    }, {
-    iconLayout: 'default#image',
-    iconImageHref: 'img/sangeethasundar.png',
-    iconImageSize: [50, 50],
-    iconImageOffset: [-3, -42]        
-    });
+    campers = [kuznetsovandrey76, andrei_u, sangeethasundar, lesedi, heinhoang, eightthday, curiouscc, alecia];
 
-	curiouscc = new ymaps.Placemark([51.538417, -0.009306], {
-    	hintContent: '@curiouscc', 
-        balloonContent: 'Silvana P' 
-    }, {
-    iconLayout: 'default#image',
-    iconImageHref: 'img/curiouscc.png',
-    iconImageSize: [50, 50],
-    iconImageOffset: [-3, -42]        
-    });
-
-	lesedi = new ymaps.Placemark([-33.918861, 18.423300], {
-    	hintContent: '@lesedi', 
-        balloonContent: 'Lesedi Diamond Damane' 
-    }, {
-    iconLayout: 'default#image',
-    iconImageHref: 'img/lesedi.png',
-    iconImageSize: [50, 50],
-    iconImageOffset: [-3, -42]        
-    });
-// (end)
-
-// (start) how to add loop in this block?
-    myMap.geoObjects.add(kuznetsovandrey76);
-    myMap.geoObjects.add(andrei_u);
-    myMap.geoObjects.add(sangeethasundar);
-    myMap.geoObjects.add(curiouscc);
-    myMap.geoObjects.add(lesedi);
-// (end)    
+    for (var i = 0; i < campers.length; i++) {
+        myMap.geoObjects.add(campers[i]);
+    }
 }
