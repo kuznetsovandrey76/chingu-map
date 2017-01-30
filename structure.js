fbb0f0b62a2a0ba892a49fcb2d7c69ce26e8b2a6
Kuznetsov Andrey <and.rey.q@yandex.ru>
2017-01-30 21:12:58 +0300

    Create structure.js

---
+++ structure.js
@@ -0,0 +1,41 @@
+var User = function(lat, lng, title, name, cohort) {
+	this.lat = lat;
+	this.lng = lng;
+	this.name = name;
+	this.title = '@' + title;
+	this.img = 'img/' + title + '.jpe'; 
+	this.cohort = cohort;
+};
+
+var kuznetsovandrey76 = new User (57.185569, 39.396566, "kuznetsovandrey76", "Kuznetsov Andrey", "cheetah");
+var andrei_u = new User (45.7489, 21.2087, "andrei_u", "Uzun Andrei", "cheetah");
+var curiouscc = new User (51.538417, -0.009306, "curiouscc", "Silvana P", "cheetah");
+var lesedi = new User (-33.918861, 18.4233, "lesedi", "Lesedi Diamond Damane", "cheetah");
+var heinhoang = new User (10.691048, 107.246215, "heinhoang", "Hein Hoang", "cheetah");
+var eightthday = new User (52.9835189, -0.0179148, "eightthday", "Toni Gibbons", "cheetah");
+var alecia = new User (32.2177, -82.4135, "alecia", "Alecia", "cheetah");
+var chance = new User (43.4643, -80.5204, "chance", "Chance Taken", "cheetah");
+var marieanabel = new User (45.748333, 4.846667, "marieanabel", "Anabel Salomone", "cheetah");
+var jose = new User (51.556, 0.173, "jose", "Jose EseEme", "cheetah");
+var vigneshblue = new User (13.068236, 80.270717, "vigneshblue", "Vigneshwaran Sivalingam", "cheetah");
+var alexbass8888 = new User (19.07283, 72.88261, "alexbass8888", "Alex Bass", "cheetah");
+var sangeethasundar = new User (13.085716, 80.208596, "sangeethasundar", "Sangeetha Sundar", "cheetah");
+var ans123 = new User (17.353389, 78.501972, "ans123", "Mansoor Ahmed", "cheetah");
+var jamshid = new User (36.313028, 59.655861, "jamshid", "Jamshid Ajam", "cheetah");
+var ivan_schwarz = new User (51.053576, 3.724607, "ivan_schwarz", "Ivan Schwarz", "cheetah");
+var johnnyzabala = new User (18.47156, -69.891372, "johnnyzabala", "Johnny Zabala", "articFox");
+var erodriguezfeliz = new User (18.4861, -69.9312, "erodriguezfeliz", "Angel Rodriguez", "rhino");
+var finrodfelagund_ = new User (45.809923, 15.987227, "finrodfelagund_", "Marko Žderić", "cheetah");
+var kornil = new User (45.070148, 11.789544, "kornil", "Francesco Agnoletto", "redPanda");
+var barbosabyte = new User (40.6358423, -7.3921798, "barbosabyte", "André Barbosa", "redPanda");
+var isaacabrahamson = new User (41.769999, -91.128358, "isaacabrahamson", "Isaac Abrahamson", "redPanda");
+var rledford = new User (30.2366, -93.3774, "rledford", "Ryan Ledford", "redPanda");
+var fobos531 = new User (46.334545, 16.614657, "fobos531", "Jakov Glavina", "cheetah");
+var ali = new User (29.761993, -95.366302, "ali", "Ali C", "redPanda");
+var jacksonbates = new User (-37.708, 145.115, "jacksonbates", "Jackson Bates", "redPanda");
+var dante = new User (45.392723, 26.321472, "dante", "Alexandru Ungureanu", "cheetah");
+var eugpoloz = new User (59.9343, 30.3351, "eugpoloz", "Evgenia Polozova", "rhino");
+var reecelangerock = new User (41.898604, -87.666735, "reecelangerock", "Reece Langerock", "redPanda");
+
+
+
